c38e3cc7e5199a4c9695ea978878c00dbe19d068
Abhishek A <abhi.misc244@gmail.com>
2025-04-21 11:30:53 +0800

    Added Excel input file

--- pandapowerTest1.py
+++ pandapowerTest1.py
@@ -5,8 +5,18 @@ import pandas as pd
 import numpy as np
 
 
-
 #add the full dataframe
-df = pd.read_excel('Load List.xlsx')
+df = pd.read_excel('Load List.xlsx', sheet_name="Buses")
+
+# Filter the full dataframe with only busses
+bus_df = df[df['Type']=='Bus']
 
 
+# Create empty network
+net = pp.create_empty_network()
+
+# Creating the busses from the dataframe
+for index, row in bus_df.iterrows():
+    pp.create_bus(net, name=row['Description'], vn_kv=row['Voltage']*.001, type="b")
+    
+print (net.bus)
